83e70be9cab1189c0d6f863718bb961f9325aa68
hpi-classroom <75154074+hpi-classroom@users.noreply.github.com>
2022-01-16 10:44:09 +0100

    Modified test case for simple web nlg experiment;

--- src/test/end_to_end/test_with_trainer.py
+++ src/test/end_to_end/test_with_trainer.py
@@ -15,6 +15,7 @@ class MyTestCase(unittest.TestCase):
         self.tokenizer = T5Tokenizer.from_pretrained(self.config["PRE_TRAINED_MODEL"])
 
     def test_webnlg_t5_base(self):
+
         config_dataset = load_config_from_yaml(self.config["DATASET_CONFIG"])
         self._train(config_dataset)
 
@@ -27,7 +28,7 @@ class MyTestCase(unittest.TestCase):
             inputs.append(item[0][0])
 
         tokenized_input = self.tokenizer.batch_encode_plus(
-            inputs, padding='max_length', max_length=2000
+            inputs, padding='max_length', max_length=500
         )
 
         labels = []
@@ -35,7 +36,7 @@ class MyTestCase(unittest.TestCase):
             labels.append(item[0])
 
         tokenized_labels = self.tokenizer.batch_encode_plus(
-            labels, padding='max_length', max_length=2000
+            labels, padding='max_length', max_length=500
         )
         tokenized_input['labels'] = tokenized_labels["input_ids"]
 
@@ -43,6 +44,7 @@ class MyTestCase(unittest.TestCase):
 
     def _train(self, config_dataset):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        batch_size = 4
 
         model = T5ForConditionalGeneration.from_pretrained("t5-base")
         model.to(device)
@@ -64,8 +66,10 @@ class MyTestCase(unittest.TestCase):
         val_dataset = val_dataset.rename_column(config_dataset["INPUT_IDS"], 'input_ids')
         val_dataset = val_dataset.rename_column(config_dataset["LABELS"], 'labels')
 
-        train_dataset = train_dataset.map(self._tokenize, batched=True, batch_size=8)
-        val_dataset = val_dataset.map(self._tokenize, batched=True, batch_size=8)
+        train_dataset.shard(num_shards=4, index=0)
+
+        train_dataset = train_dataset.map(self._tokenize, batched=True, batch_size=batch_size)
+        val_dataset = val_dataset.map(self._tokenize, batched=True, batch_size=batch_size)
 
         for feature in train_dataset.features:
             if feature not in ['input_ids', 'attention_mask', 'labels']:
@@ -83,9 +87,9 @@ class MyTestCase(unittest.TestCase):
         training_args = TrainingArguments(
             output_dir=output_dir,
             num_train_epochs=10,
-            per_device_train_batch_size=8,
-            per_device_eval_batch_size=8,
-            eval_accumulation_steps=0,  # Number of eval steps to keep in GPU (the higher, the mor vRAM used)
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            eval_accumulation_steps=1,  # Number of eval steps to keep in GPU (the higher, the mor vRAM used)
             prediction_loss_only=True,  # If I need to compute only loss and not other metrics-> will use less RAM
             learning_rate=0.001,
             evaluation_strategy=IntervalStrategy.STEPS,  # Run evaluation every eval_steps
@@ -108,7 +112,7 @@ class MyTestCase(unittest.TestCase):
         trainer.train()
         print("finished training")
         print("saving model")
-        trainer.save_model(output_dir + '/model')
+        trainer.save_model(join("models", output_dir))
 
 
 if __name__ == '__main__':
