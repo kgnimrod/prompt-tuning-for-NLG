b0d9bde3706c60da5bf8bdcc2813b04a1744e529
Furkan <furkan.simsek@student.hpi.uni-potsdam.de>
2022-01-25 17:15:13 +0100

    Fixed the BERT Score calculation and updated the official_evaluation_script_scores.md accordingly. Added remaining soft prompts to subdirectory soft_prompts.

--- t5-tuning/web_nlg_amr_evaluation/GenerationEval-master/eval.py
+++ t5-tuning/web_nlg_amr_evaluation/GenerationEval-master/eval.py
@@ -246,7 +246,7 @@ def bert_score_(references, hypothesis, lng='en'):
     try:
         P, R, F1 = score(hypothesis, references, lang=lng)
         logging.info('FINISHING TO COMPUTE BERT SCORE...')
-    #     print('FINISHING TO COMPUTE BERT SCORE...')
+        print('FINISHING TO COMPUTE BERT SCORE...')
         P, R, F1 = list(P), list(R), list(F1)
         F1 = float(sum(F1) / len(F1))
         P = float(sum(P) / len(P))
@@ -312,7 +312,8 @@ if __name__ == '__main__':
     argParser.add_argument("-H", "--hypothesis", help="hypothesis translation", required=False)
     argParser.add_argument("-lng", "--language", help="evaluated language", default='en')
     argParser.add_argument("-nr", "--num_refs", help="number of references", type=int, default=4)
-    argParser.add_argument("-m", "--metrics", help="evaluation metrics to be computed", default='bleu,meteor,ter,chrf++,bleurt')
+    argParser.add_argument("-m", "--metrics", help="evaluation metrics to be computed", default='bleu,meteor,ter,chrf++,bert,bleurt')
+
     argParser.add_argument("-nc", "--ncorder", help="chrF metric: character n-gram order (default=6)", type=int, default=6)
     argParser.add_argument("-nw", "--nworder", help="chrF metric: word n-gram order (default=2)", type=int, default=2)
     argParser.add_argument("-b", "--beta", help="chrF metric: beta parameter (default=2)", type=float, default=2.0)
@@ -320,12 +321,12 @@ if __name__ == '__main__':
     args = argParser.parse_args()
 
     logging.info('READING INPUTS...')
-    refs_path = '../../data/web_nlg/test/reference'
-    hyps_path = '../../data/web_nlg/test/prompt_tuning_hypothesis/hypothesis'
+    refs_path = '../../data/amr/test/reference'
+    hyps_path = '../../data/amr/test/fine_tuning_hypothesis/hypothesis'
+
     lng = args.language
     num_refs = args.num_refs
     metrics = args.metrics#.lower().split(',')
-
     nworder = args.nworder
     ncorder = args.ncorder
     beta = args.beta
