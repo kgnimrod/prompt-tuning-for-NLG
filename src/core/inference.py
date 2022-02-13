d7352e029340d879ac738a033787f8f680838869
hpi-classroom <75154074+hpi-classroom@users.noreply.github.com>
2022-02-13 22:58:02 +0100

    Modified: make predictions logic updated to work with inputs from hugginface datasets;
    Added: experiment to make predictions on amr_e2e_web mixed inputs;

--- src/core/inference.py
+++ src/core/inference.py
@@ -3,35 +3,29 @@ from itertools import chain
 import torch
 
 
-def make_predictions(model, encoding, tokenizer, use_embeddings=False):
+def make_predictions(model, encoding, tokenizer):
     model_predictions = []
     model.eval()
     with torch.no_grad():
-        for i in range(len(encoding)):
-            print('make_predictions: encoding: ', i)
-            input_ids = encoding[i]['input_ids']
-            attention_mask = encoding[i]['attention_mask']
+        input_ids = encoding['input_ids']
+        # attention_mask = encoding[i]['attention_mask']
 
-            args = {
-                'input_ids': input_ids,
-                # 'decoder_input_ids': input_ids,
-                'max_length': 400,
-                'bos_token_id': 0,
-                'pad_token_id': 0,
-                'eos_token_id': 1,
-                'use_cache': True,
-                'attention_mask': attention_mask
-            }
-            if use_embeddings:
-                args['inputs_embeds'] = model.embed_tokens(input_ids)
-                args['decoder_input_ids'] = input_ids
-                args['attention_mask'] = model.extend_attention_mask(attention_mask)
-                del args['input_ids']
+        args = {
+            'input_ids': input_ids,
+            # 'decoder_input_ids': input_ids,
+            'do_sample': True,
+            'max_length': 500,
+            # 'bos_token_id': 0,
+            # 'pad_token_id': 0,
+            # 'eos_token_id': 1,
+            # 'use_cache': True,
+            'top_k': 50
+        }
 
-            output = tokenizer.batch_decode(
-                model.generate(**args),
-                skip_special_tokens=True)
-            model_predictions.append([x.replace('<pad>', '').replace('</s>', '').strip() for x in output])
+        output = tokenizer.batch_decode(
+            model.generate(**args),
+            skip_special_tokens=True)
+        model_predictions.append([x.replace('<pad>', '').replace('</s>', '').strip() for x in output])
 
         # flatten the predictions list which has the length of batch_size * number_of_batches
         model_predictions = list(chain(*model_predictions))
