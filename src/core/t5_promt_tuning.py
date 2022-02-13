c3a2ccad898b6b27173ae8b5fc3fc2098c67c59a
hpi-classroom <75154074+hpi-classroom@users.noreply.github.com>
2022-02-13 20:13:35 +0100

    Modified: fix cuda device; Modified: changed training method to huggingface, modified t5_promt_tuning.py to add soft prompt in forward step;

--- src/core/t5_promt_tuning.py
+++ src/core/t5_promt_tuning.py
@@ -15,6 +15,7 @@ class T5PromptTuning(T5ForConditionalGeneration):
     def from_pretrained(cls, model_name_or_path: str, soft_prompt_path: str = None, number_tokens: int = None,
                         initialize_from_vocab: bool = True, random_range: float = 0.5, **kwargs):
 
+        print("model created with T5PromptTuning.from_pretrained")
         model = super().from_pretrained(model_name_or_path, **kwargs)
 
         #  freeze the transformers model
@@ -42,34 +43,34 @@ class T5PromptTuning(T5ForConditionalGeneration):
     def initialize_soft_prompt(self, number_tokens: int = 20, initialize_from_vocab: bool = True, random_range: float = 0.5):
         self.number_tokens = number_tokens
         if initialize_from_vocab:
-            init_prompt_value = self.shared.weight[:number_tokens].clone().detach()
+            init_prompt_value = (
+                self.get_input_embeddings().weight[:number_tokens].clone().detach()
+            )
         else:
             init_prompt_value = torch.FloatTensor(number_tokens, self.config.d_model)\
                 .uniform_(-random_range, random_range)
 
-        print(init_prompt_value.shape)
-        print(self.shared.weight.shape)
-
+        self.soft_prompt = torch.nn.Embedding(number_tokens, self.config.d_model)
         # Initialize weight
-        self.soft_prompt = torch.nn.parameter.Parameter(init_prompt_value)
+        self.soft_prompt.weight = torch.nn.parameter.Parameter(init_prompt_value)
 
     def get_soft_params(self):
         return self.soft_prompt
 
     # this method appends the learned prompt embeddings to the input ids of the input before forward pass is calculated
     def embed_tokens(self, input_ids):
-        self.shared.to(self.device)
-        self.soft_prompt.to(self.device)
-        inputs_embeds = self.shared(input_ids)
+        inputs_embeds = self.get_input_embeddings()(input_ids)
+        #         inputs_embeds = self.transformer.wte(input_ids)
 
         if len(list(inputs_embeds.shape)) == 2:
             inputs_embeds = inputs_embeds.unsqueeze(0)
 
-        # the shape of the tensor that will be returned will be:
-        # [batch_size, max_sequence_length, number_embeddings] -> [8, 600, 512]
-        learned_embeds = self.soft_prompt.repeat(inputs_embeds.size(0), 1, 1)
-        learned_embeds.to(self.device)
-        return torch.cat([learned_embeds, inputs_embeds], dim=1)
+        # [batch_size, n_tokens, n_embd]
+        learned_embeds = self.soft_prompt.weight.repeat(inputs_embeds.size(0), 1, 1)
+
+        inputs_embeds = torch.cat([learned_embeds, inputs_embeds], dim=1)
+
+        return inputs_embeds
 
     # to make sure that padding token ids of the labels are not taken into account by the loss function
     # this method extends the label's tensor by elements that are ignored by the CrossEntropyLoss function
@@ -100,3 +101,66 @@ class T5PromptTuning(T5ForConditionalGeneration):
             dim=1
         )
 
+    def forward(
+            self,
+            input_ids=None,
+            past_key_values=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            encoder_hidden_states=None,
+            encoder_attention_mask=None,
+            encoder_outputs=None,
+            decoder_input_ids=None,
+            decoder_head_mask=None,
+            decoder_attention_mask=None,
+            cross_attn_head_mask=None,
+            labels=None,
+            use_cache=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+            to_encoder_only=False,
+    ):
+        if input_ids is not None:
+            inputs_embeds = self.embed_tokens(input_ids).to(
+                self.device
+            )
+
+        if labels is not None:
+            labels = self.extend_labels(labels).to(self.device)
+
+        # for training, extend the attention mask to include input embeddings, but not for inference,
+        # where greedy search only requires encoder outputs and decoder_input ids and the shape needs to match
+        if attention_mask is not None:
+            attention_mask = self.extend_attention_mask(attention_mask).to(self.device)
+            if decoder_attention_mask is not None:
+                decoder_attention_mask = self.extend_attention_mask(
+                    decoder_attention_mask
+                ).to(self.device)
+
+        if to_encoder_only:
+            return self.encoder(inputs_embeds=inputs_embeds, return_dict=True)
+
+        # for inference (i.e. generate) - build pipeline for generate function
+        if decoder_input_ids is not None:
+            return super().forward(
+                inputs_embeds=inputs_embeds,
+                decoder_input_ids=decoder_input_ids,
+                encoder_outputs=encoder_outputs,
+                use_cache=use_cache,
+                return_dict=return_dict,
+            )
+
+        # for training
+        return super().forward(
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            labels=labels,
+            decoder_attention_mask=decoder_attention_mask,
+            use_cache=use_cache,
+            return_dict=return_dict,
+            encoder_outputs=encoder_outputs,
+        )
