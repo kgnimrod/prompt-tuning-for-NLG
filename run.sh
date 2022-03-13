d42e4d68d87ee1f9f6560e35fc6eac1835705a7f
hpi-classroom <75154074+hpi-classroom@users.noreply.github.com>
2022-03-13 06:12:09 +0100

    MODIFIED: - wandb;

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 pip install -r requirements
-python3 main.py --experiment "amr_nlg_t5_small_prompt_tuning.yml"
+python3 main.py --experiment "web_nlg_t5_small_prompt_tuning.yml"
--- src/core/evaluation.py
+++ src/core/evaluation.py
@@ -30,6 +30,7 @@ def predict(tokenizer, model, loader, embeddings=None):
             if embeddings is not None:
                 args["inputs_embeds"] = embeddings.extend_inputs(ids).to(device)
                 args["attention_mask"] = embeddings.extend_attention_mask(mask).to(device)
+                args["decoder_inputs_embeds"] = embeddings.extend_inputs(ids).to(device)
             else:
                 args["input_ids"] = ids
 
@@ -38,10 +39,10 @@ def predict(tokenizer, model, loader, embeddings=None):
             # Decode y_id and prediction #
             prediction = [
                 tokenizer.decode(
-                    p, skip_special_tokens=True
+                    p, skip_special_tokens=True, clean_up_tokenization_spaces=False
                 ) for p in raw_prediction
             ]
-            target = [tokenizer.decode(t, skip_special_tokens=True) for t in y_id]
+            target = [tokenizer.decode(t, skip_special_tokens=True, clean_up_tokenization_spaces=False) for t in y_id]
 
             print(prediction)
             print(target)
